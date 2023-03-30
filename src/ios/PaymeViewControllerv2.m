f875b2c350360ca6f076d6cec7f871930b441571
edaviok1301 <eparedesba1301@gmail.com>
2023-03-30 10:32:20 -0500

    Update PaymeViewControllerv2.m

--- src/ios/PaymeViewControllerv2.m
+++ src/ios/PaymeViewControllerv2.m
@@ -42,7 +42,6 @@ NSString  *operationNumberG = nil;
     dispatch_async(dispatch_get_main_queue(), ^{
         UIViewController *uvc = [[[UIApplication sharedApplication] keyWindow] rootViewController];
 
-        NSLog(@"%@", [@"key:" stringByAppendingString:[self.request objectForKey:@"identifier"]]);
         PaymeClient * pc = [[PaymeClient alloc] initWithDelegate:self key:[self.request objectForKey:@"identifier"]];
         [pc setEnvironmentWithEnvironment:[self setEnviroment]];
         [pc authorizeTransactionWithController:uvc usePresent:true paymeRequest:[self setParamsMerchant:request]];
